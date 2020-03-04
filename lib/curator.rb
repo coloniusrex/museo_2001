b8e4781cfbf88b277ac8255807a4d00e72ede4ae
Colin Alexander <colintalex@gmail.com>
2020-03-04 10:37:10 -0700

    Adds 3 methods for locationg artists/photos

--- lib/curator.rb
+++ lib/curator.rb
@@ -17,4 +17,24 @@ attr_reader :photographs, :artists
   def find_artist_by_id(id)
     @artists.find { |artist| artist.id == id}
   end
+
+  def photographs_by_artist
+    @artists.reduce({}) do |artist_photos, artist|
+      find = @photographs.find_all { |photo| photo.artist_id == artist.id}
+      artist_photos[artist] = find
+      artist_photos
+    end
+  end
+
+  def artists_with_multiple_photographs
+    photographs_by_artist.select do |artist, photos|
+       photos.length > 1
+    end.keys
+  end
+
+  def photographs_taken_by_artist_from(country)
+    a = photographs_by_artist.select do |artist, photos|
+      artist.country == country
+    end.values.flatten
+  end
 end
